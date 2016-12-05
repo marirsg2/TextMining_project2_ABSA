44cb116ba8f65563a28c49c1b5e1eddb93db2647
marirsg2 <marirsg2@gmail.com>
2016-12-05 02:00:36 -0500

    untested code for extracting the adj lemma

--- src/wordNetAspectPolarityDetection.py
+++ src/wordNetAspectPolarityDetection.py
@@ -6,6 +6,8 @@ Created on Dec 2, 2016
 
 from PyDictionary import PyDictionary
 from nltk.corpus import wordnet as wn
+import nltk.stem
+import math
 
 # dictionary = PyDictionary()
 # print(dictionary.googlemeaning("delighted"))
@@ -26,6 +28,8 @@ neutralWords = [ "normal", "neutral", "ok", "o.k.", "okay", ]
 
 adjectivePolaritySwitchers = ['not', 'never', 'over', 'excess']
 
+NO_VALID_ADDRESS = -1
+
 #also if the words are not positive or negative, then it is given neutral polarity.
 #------------------------------------------------------------------------------ 
 # get the polarity of adjective words from the training data
@@ -43,13 +47,19 @@ trainingDataSeedWords = {} #
 #===============================================================================
 def unsupervisedWordNetPolarity_updateDictWithAspectPolarityPairs(allRestaurantData):
     '''
-    @summary: Now load the reviews with deps parsing, and ASSUME the aspect is known. Find the adjective
+    @summary: Now load the reviews with deps parsing, and ASSUME the aspect is known. 
+
+Find the adjective
 qualifying the aspect. Then apply the dictionary search. Can also be an adverb , if the aspect is the noun subject eg: food is great.
+
 NOTE: if no amod or nsubj connection, find the nearest JJ that has the "head" as the aspect and not connected to anything else
-NOTE: if the aspect is not literally there in the sentence, then this approach cannot work
 IMPORTANT: Can be "little perks were great" little is amod, so is great. Read through and id all JJ with nsubj as aspect.
+
+NOTE: if the aspect is not literally there in the sentence, then this approach cannot work
+
 NOTE: if the adj is not linked in DEPS tags, then look for the adjectives around the aspect word,
 IMPORTANT: look for negation words with 2 words before it ? Or can you use the tree?
+
 dictionary search: Get the definition, do dependency parsing.
         Then for each adjective, 
             a) see if it is negated. 
@@ -65,12 +75,92 @@ dictionary search: Get the definition, do dependency parsing.
         "Decor is nice and minimalist, food simple yet very well presented and cooked, and the wine list matches the food very well"
                 
     '''
+    lemmatizer = nltk.stem.WordNetLemmatizer()
+    
     #each review is a dict that contains the raw data, POS tags, DEPS (dependency parser) tags, and the 
     # actual aspects, polarity, and category for each review
     for singleReview in allRestaurantData['sentences']['sentence']:
         #first get the aspects in the sentence
-        aspectList = []
-        print("a")
+        aspectTerms = singleReview['aspectTerms']['aspectTerm']
+        if type(aspectTerms) == dict:
+            # if there were more than one aspect term, it would be in a list
+            #this just makes the single term case (which would be a dict ) into a list of one
+            # so the code is common
+            aspectTerms = [aspectTerms]            
+        for singleAspectTermDict in aspectTerms:
+            aspectWord = singleAspectTermDict['@term']
+            truePolarity = singleAspectTermDict['@polarity']
+            #find the DEPS address(es), the word can appear more than once
+            singleAspectDepsAddresses = []
+            for depsSentenceIndex in range(0,len(singleReview['DEPStagging'])):
+                depsSentence = singleReview['DEPStagging'][depsSentenceIndex ]
+                for depsNode in depsSentence[1:] : #we skip the first one which is the root node
+                    if depsNode['word'] == aspectWord:
+                        amodAddress =  NO_VALID_ADDRESS # -1 means no address
+                        try:
+                            amodAddress = depsNode['deps']['amod']
+                        except:
+                            pass  
+                        #append the tuple of the aspect word and the adj modifier
+                        singleAspectDepsAddresses.append((depsSentenceIndex,depsNode['address'],amodAddress))                                             
+                #END FOR loop through the deps nodes
+            #END FOR loop through the deps sentences
+            #------------------------------------------------------------------------------ 
+            #NOW determine the polarity of the aspect word
+            qualifyingWordTuples = [] # each entry is (sentenceIndex, wordAddress, word)
+            for (sentenceIndex, aspectAddress,amodAddress) in singleAspectDepsAddresses:                                
+                #note the deps parser address does not always match the list index. so iterate through nodes 
+                for singleNode in singleReview['DEPStagging'][sentenceIndex]:
+                    #CHECK if the node matches our adjective mod address
+                    if singleNode['address'] == amodAddress: #If amod was -1, then we wont find one
+                        if singleNode['POS'] not in ["JJ", "JJR", "JJS"]:
+                            print("Error , incorrect DEPS tree connection with amod")
+                        else:
+                            qualifyingWordTuples.append( (sentenceIndex, singleNode['word'],singleNode['POS']) )
+                    #end if the address was the adj modifier address
+                    elif singleNode['head'] == aspectAddress:
+                        qualifyingWordTuples.append((sentenceIndex, singleNode['word'],singleNode['POS']))
+                    #if it is an adjective , check if it was within 3 distance away and NOT pointing to another nsubj
+                    elif singleNode['POS'] in ["JJ", "JJR", "JJS"]:
+                        if  abs(singleNode['address'] -  aspectAddress) <= 3:
+                            #check if it does not modify another noun
+                            try:
+                                if singleNode['deps']['nsubj'] == aspectAddress:                                            
+                                    qualifyingWordTuples.append((sentenceIndex, singleNode['word'],singleNode['POS']))
+                                    continue
+                                else:
+                                    continue #the nsubj is another, skip this word, back to for loop
+                            except:
+                                pass
+                            try:
+                                if singleNode['deps']['nmod'] == aspectAddress:                                            
+                                    qualifyingWordTuples.append((sentenceIndex, singleNode['word'],singleNode['POS']))
+                                    continue
+                                else:
+                                    continue #the nsubj is another, skip this word, back to for loop
+                            except:
+                                pass
+                            #if we made it here for this case, then the adjective is close to the aspect , and does not qualify
+                            # any other noun, then take it as valid
+                            qualifyingWordTuples.append((sentenceIndex, singleNode['word'],singleNode['POS']))
+                        #END if the word is within 3 addresses
+                    #END elif singleNode['POS'] in ["JJ", "JJR", "JJS"]:
+                #END FOR loop through the DEPS node
+            #END FOR loop through the singleAspectDepsAddresses
+            #------------------------------------------------------------------------------ 
+            #At this point we have all the qualifying words for the aspect
+            for singleQualifierTuple in qualifyingWordTuples:
+                lemmaWord = lemmatizer.lemmatize(singleQualifierTuple[1], singleQualifierTuple[2])
+                
+                            
+                            
+                                
+                                     
+                        
+
+  
+                             
+             
         
         
 
