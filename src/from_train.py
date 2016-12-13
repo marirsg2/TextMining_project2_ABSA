3abcb87fa59f797d721fa42b5c125f571c551d08
marirsg2 <marirsg2@gmail.com>
2016-12-12 22:48:43 -0500

    included song's results

--- src/from_train.py
+++ src/from_train.py
@@ -32,7 +32,7 @@ class FromTrain:
                     for aspect in aspects:
                         self.__plus_one(aspect['@term'])
 
-        print ('%d' % cnt + ' reviews counted')
+        #print ('%d' % cnt + ' reviews counted')
         return self.aspects
         return sorted(self.aspects.items(), key=operator.itemgetter(1), reverse = True)
 
@@ -53,7 +53,8 @@ class FromTrain:
         for aspect in self.aspects:
             aspects[aspect] = float(aspects[aspect]) / float(self.aspects[aspect])
             if aspects[aspect] > 1.0:
-                print(aspect, self.aspects[aspect], aspects[aspect])
+                #print(aspect, self.aspects[aspect], aspects[aspect])
+                pass
 
         return aspects
 
@@ -63,7 +64,7 @@ class FromTrain:
         # print ('screen' in aspects)
         # print (len(aspects))
         test_aspects = self.extract_aspects(test_source)
-        print (len(test_aspects))
+        #print (len(test_aspects))
 
         cnt = 0
         for aspect in test_aspects:
@@ -73,8 +74,8 @@ class FromTrain:
             #    print (aspect)
 
         #print (len(aspects))
-        print (cnt)
-        print (len(test_aspects))
+#         print (cnt)
+#         print (len(test_aspects))
 
         return float(cnt) / float(len(test_aspects))
 
@@ -105,7 +106,8 @@ class FromTrain:
                         for pos in word['deps']['compound']:
                             tmpword = self.__get_word_by_address(dep, pos)['word'] + ' ' + tmpword
                         if len(word['deps']['compound']) > 1:
-                            print (tmpword)
+                            #print (tmpword)
+                            pass
                     for pos in word['deps']['amod']:
                         if tmpword in polarities:
                             p = polarities[tmpword]
@@ -128,7 +130,13 @@ class FromTrain:
         for adj, v in adjs.items():
             res[adj] = (float(-v[0] + v[2])) / (float(v[0] + v[1] + v[2]))
         return res
-
+    
+    def get_aspect_polarity(self):
+        asps = self.get_asp_polarity_count()
+        res = {}
+        for asp, v in asps.items():
+            res[asp] = (float(-v[0] + v[2])) / (float(v[0] + v[1] + v[2]))
+        return res
 
     def get_adj_polarity_count(self):
         pair_res = self.get_pair_polarity()
@@ -137,5 +145,60 @@ class FromTrain:
             if adj[0] not in adjs:
                 adjs[adj[0]] = [0,0,0]
             adjs[adj[0]][adj[2] + 1] += 1
-
         return adjs
+    
+    def get_asp_polarity_count(self):
+        pair_res = self.get_pair_polarity()
+        asps = {}
+        for asp in pair_res:
+            if asp[1] not in asps:
+                asps[asp[1]] = [0,0,0]
+            asps[asp[1]][asp[2] + 1] += 1
+        return asps   
+
+    def getAccuracy(self,trainedList, testReviews):
+        '''
+        '''
+        cnt = 0;
+        correctCount = 0
+        
+#        trainedList= set(trainedList)
+#         countingDict = {}
+#         for singleEntry in trainedList:
+#             try:
+#                 countingDict[singleEntry]
+            
+        for singleReview in testReviews['sentences']['sentence']:
+        #first get the aspects in the sentence        
+            try:
+                aspectTerms = singleReview['aspectTerms']['aspectTerm']
+            except:
+                continue #no aspect terms        
+            if type(aspectTerms) == dict:
+                # if there were more than one aspect term, it would be in a list
+                #This following line of code just makes the single term case (which would be a dict ) into a list of one
+                # so the code is common
+                aspectTerms = [aspectTerms]
+            singleReview['dictAspectPolarity'] = {} #prepare to store the results            
+            for singleAspectTermDict in aspectTerms:
+                cnt+=1
+                aspectWord = singleAspectTermDict['@term']  
+                truePolarity = singleAspectTermDict['@polarity']
+                if truePolarity == "positive":
+                    truePolarity =1
+                elif truePolarity == "negative":
+                    truePolarity =-1
+                elif truePolarity == "neutral":
+                    truePolarity =0           
+                else:                
+                    continue    # we currently dont handle conflict cases                
+                for singleEntry,value in trainedList.items():
+                    if singleEntry == aspectWord and (truePolarity == 0) == (value == 0) and (truePolarity > 0) == (value > 0):
+                        correctCount +=1
+            #END for loop through aspects
+        #END for loop through reviews
+        
+        print("The accuracy is = ", correctCount/cnt )
+                        
+    
+    
--- src/mainScript.py
+++ src/mainScript.py
@@ -12,6 +12,7 @@ import file_operations
 from from_train import FromTrain
 import nltkWordnetAspectExtraction as lexicalApproach
 import wordNetAspectPolarityDetection
+#import evaluation_task1_freq_tfidf as eval
 
 
 #IMPORTANT: please run preProcess.py before the main script. Ensure you update the location of the 
@@ -19,7 +20,6 @@ import wordNetAspectPolarityDetection
 (restaurantTrainDict,restaurantTestDict,laptopTrainDict, laptopTestDict) = \
                                 file_operations.loadAndGetRawDataFromPickle(pickleFolder, pickleFile)
 #at this point you have all your training and test data in python dictionaries. Run algorithms
-
 
 allRestaurantData = copy.deepcopy(restaurantTrainDict)
 allRestaurantData['sentences']['sentence'] = allRestaurantData['sentences']['sentence'] + restaurantTestDict['sentences']['sentence']
@@ -35,8 +35,8 @@ allLaptopData ['sentences']['sentence'] = allLaptopData['sentences']['sentence']
 #---------UNSUPERVISED TECHNIQUES-------------
 
 #1) Doc 2 Vec (improvement on word 2 vector for training documents with labels)
-#The doc2vec approach has been commented out because you need to first install gensim library
-#also uncomment the import line
+'''The doc2vec approach has been commented out because you need to first install gensim library
+also uncomment the import line. See the script for a detailed description'''
 # doc2VecResults = \
 #         doc2VecAspectIdentification.getDictionaryOfAspectsByCategory({"laptop":allLaptopData,"restaurant":allRestaurantData})
 #print (doc2VecResults)
@@ -44,9 +44,11 @@ allLaptopData ['sentences']['sentence'] = allLaptopData['sentences']['sentence']
 #-----------------------------------------------
 
 #2) wordNet definitions relationships for aspect identifications
-#This approach tried to identify aspects by finding nouns that were in the definitions of words that were related to the review
-# main categories, namely "laptop" and "restaurant". This failed because there definitions, synonyms, meronyms (part relationships)
-# were incomplete or sparse in wordnet. The script is nltkWordnetAspectExtraction.py
+'''
+This approach tried to identify aspects by finding nouns that were in the definitions of words that were related to the review
+ main categories, namely "laptop" and "restaurant". This failed because there definitions, synonyms, meronyms (part relationships)
+ were incomplete or sparse in wordnet. The script is nltkWordnetAspectExtraction.py
+ '''
 
 #-----------------------------------------------
 
@@ -54,20 +56,13 @@ allLaptopData ['sentences']['sentence'] = allLaptopData['sentences']['sentence']
 
 #---------------------SUPERVISED TRAINING-------------------------
 
+supervised_laptopTrain = FromTrain(laptopTrainDict)
+supervised_restaurantTrain = FromTrain(restaurantTrainDict)
+#need py 3.5 to run this function. If not, for results see writeup
+#Also uncomment the import of evaluation_task1_freq_tfidf 
+#eval.evaluate2();
 
-#===================================================================================
-#from_train = FromTrain(allLaptopData)
-#from_train = FromTrain(laptopTrainDict)
-#aspects = from_train.extract_aspects()
-#res = from_train.aspects_performance(laptopTestDict)
-#res = from_train.aspects_performance(laptopTrainDict)
-#res = from_train.aspects_performance(allLaptopData)
-#res = from_train.aspects_performance(allLaptopData)
-# res = from_train.get_pair_polarity()
-# for r in res:
-#     print (r)
-    
-#==========================TASK2 ASPECT POLARITY==================================
+print("==========================TASK2 ASPECT POLARITY==================================")
 
 #-------- UNSUPERVISED APPROACHES---------------
 # 1) Wordnet aspect polarity detection  ( in wordNetAspectPolarityDetection.py)
@@ -92,4 +87,37 @@ print("=========Results for task 2 in RESTAURANT category   for CONFLICT detecti
 wordNetAspectPolarityDetection.unsupervisedDetectConflictSentences(allRestaurantData)
 print("=============x-------------------END RESULTS FOR UNSUPERVISED TASK 2--------------x===============================")
 
-#----------------------- SUPERVISED approach to task 2-----------------------------
+print("----------------------- SUPERVISED approach to task 2-----------------------------")
+'''
+These results were actually really low because we used the dependency parser to extract the nouns 
+and their adjectives for polarity. Only if the noun matched the aspect, then we would save the nouns, 
+adjective and the polarity (from the review data). 
+The dependency parser  did not detect a majority of the aspects correctly. So our training model was weak.
+IMPROVEMENT: If we don't try and extract the aspects with the dependency parser, and instead we can
+ just take the aspects directly. This would make the model stronger as it would have all the aspects
+
+'''
+#get the noun adj pair prediction for the training data
+laptop_listOfNounAdjPairs = supervised_laptopTrain.get_pair_polarity();
+restaurant_listOfNounAdjPairs = supervised_restaurantTrain.get_pair_polarity();
+#compare with the results from the test data
+#first get the test data results
+test_laptopTrain = FromTrain(laptopTestDict)
+test_restaurantTrain = FromTrain(restaurantTestDict)
+laptop_testResultsNounAdjPairs = test_laptopTrain.get_pair_polarity();
+restaurant_testResultsNounAdjPairs = test_restaurantTrain.get_pair_polarity();
+#now compare with the train data results
+print("=====TASK 2- supervised training results for LAPTOP reviews==========")
+
+
+trained_list_laptop = supervised_laptopTrain.get_aspect_polarity()
+
+test_laptopTrain.getAccuracy(trained_list_laptop, laptopTestDict)
+
+print("=====TASK 2- supervised training results for RESTAURANT reviews==========")
+
+trained_list_restaurant = supervised_restaurantTrain.get_aspect_polarity()
+
+test_restaurantTrain.getAccuracy(trained_list_restaurant, restaurantTestDict)
+
+
